d4282db1b3ba2d54f4ae76eb126b8a1c33cde0b0
Shehroze4543 <86188771+Shehroze4543@users.noreply.github.com>
2023-07-28 14:01:50 -0400

    Shehroz latest commit

--- src/components/MainContainer.jsx
+++ src/components/MainContainer.jsx
@@ -1,18 +1,45 @@
-import React from 'react';
-import SideBar from './SideBar';
-import Page from './Page';
-
-
+import React from "react";
+import SideBar from "./SideBar";
+import Page from "./Page";
+import upload from "./images/upload.png";
 const MainContainer = (props) => {
-	
-	
-	
-	return (
-		<div className="MainContainer">
-			<SideBar />
-			<Page char={props.char} onClickFn={props.onClickFn}/>
-		</div>
-	)
-}
+  return (
+    <>
+      <div className="MainContainer">
+        <SideBar />
+        <div className="maincontainer">
+          <div className="maincont">
+            <div className="story">
+              <img src={upload} alt="" />
+              <p> Post Story</p>
+            </div>
 
-export default MainContainer
+            <div className="story">
+              <img src={upload} alt="" />
+              <p> Post Story</p>
+            </div>
+
+            <div className="story">
+              <img src={upload} alt="" />
+              <p> Post Story</p>
+            </div>
+
+            <div className="story">
+              <img src={upload} alt="" />
+              <p> Post Story</p>
+            </div>
+            <div className="story">
+              <img src={upload} alt="" />
+              <p> Post Story</p>
+            </div>
+          </div>
+          <div className="profile-sec">
+            <Page char={props.char} onClickFn={props.onClickFn} />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default MainContainer;
